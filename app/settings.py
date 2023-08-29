68c82977b91f60d353a5c5805abe63d34544d643
Arter Tendean <arter.tendean.07@gmail.com>
2023-08-29 22:33:38 +0800

    Adding `djangorestframework`

--- app/settings.py
+++ app/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # dependencies
+    "rest_framework",
     # apps
     "users",
 ]
