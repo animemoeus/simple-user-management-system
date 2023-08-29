7fbb80e7863ef2d07a415d1bae2e43a02ce7a035
Arter Tendean <arter.tendean.07@gmail.com>
2023-08-29 23:29:23 +0800

    Adding `/users/` search feature

--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from rest_framework import filters
 from rest_framework.generics import ListCreateAPIView
 
 from .models import User
@@ -8,3 +8,6 @@ from .serializers import UserSerializer
 class UserView(ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["first_name", "last_name", "address"]
