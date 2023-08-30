cf8c8aa518d33377aa2b548510e078d1c928fe37
Arter Tendean <arter.tendean.07@gmail.com>
2023-08-30 13:02:27 +0800

    Enable modal close button

--- frontend/src/app/users/addUserModal.js
+++ frontend/src/app/users/addUserModal.js
@@ -116,22 +116,13 @@ export default function Modal() {
                       <button
                         className="text-red-500 background-transparent font-bold px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                         type="button"
-                        // onClick={() => setShowModal(false)}
-                        onClick={() =>
-                          toast({
-                            description: "User Added",
-                            status: "success",
-                            isClosable: true,
-                            position: "top-right",
-                          })
-                        }
+                        onClick={() => setShowModal(false)}
                       >
                         Close
                       </button>
                       <button
                         className="bg-emerald-500 text-white active:bg-emerald-600 font-bold text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                         type="submit"
-                        // onClick={() => reset()}
                       >
                         {isLoading ? "Loading..." : "Save Changes"}
                       </button>
